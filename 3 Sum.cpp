caf8614086069a6633d43890af83d54dafb9e0e8
Niharikar Reddy <niharikar005@gmail.com>
2018-01-06 21:55:00 -0500

    Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.  Note: The solution set must not contain duplicate triplets.
    iterating through the array by fixing an element as target and searching other pair of numbers from the next element of array till the end by using "front" "back" strategy.

---
+++ 3 Sum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+vector< vector<int> > threesum(vector<int>& nums)
+{
+     vector< vector<int> > ans;
+    sort(nums.begin().nums.end());
+    for(int i = 0;i<nums.size();i++)
+    {
+        int target = -nums[i];
+        int front = i+1, back = nums.size()-1;
+        
+        while(front < back)
+        {
+            if( nums[front] + nums[back] > target )
+              back--;
+            else if( nums[front] + nums[back] < target )
+              front++;
+             else
+             {
+                  vector<int> t(3,0);
+                  t[0] = nums[front];t[1] = nums[back];t[2] = -target;
+                  ans.push_back(t);
+
+                  while( nums[front] == nums[front+1] && front!=back )
+                    front++;
+                   while( nums[back] == nums[back-1] && front!=back)
+                    back--;
+
+             }
+         }
+         
+         
+         while( nums[i] == nums[i+1] && i+1 < nums.size() )
+          i++;
+        
+    }
+    
+    return ans;
+    
+  
+    
+}
