353250732cfd17d0522040eb2fb1fe506e838f41
Niharikar Reddy <niharikar005@gmail.com>
2018-06-26 12:10:15 -0400

    Computing leftmax and rightmax in blocks of window size for array, then taking max of the appropriate elements of leftmax and rightmax arrays in the answer

--- Sliding_window_max.cpp
+++ Sliding_window_max.cpp
@@ -26,3 +26,40 @@ vector<int> maxSlidingWindow(vector<int>& nums,int k)
     
     return nums;
 }
+
+
+// another method with O(n) space complexity
+
+vector<int> maxSlidingWindow(vector<int>& nums,int k)
+{
+    vector<int> leftmax(nums.size(),0);
+    vector<int> rightmax(nums.size(),0);
+    
+    leftmax[0] = nums[0];
+    for(int i = 1;i<nums.size();i++)
+    {        
+        if(i%(k) == 0)
+            leftmax[i] = nums[i];
+        else
+            leftmax[i] = max(nums[i],leftmax[i-1]);
+
+    }
+
+    
+    rightmax[nums.size()-1] = nums[nums.size()-1];
+    for(int i = nums.size()-2;i>=0;i--)
+    {
+        if(i%k == 0)
+            rightmax[i] = nums[i];
+        else
+            rightmax[i] = max(nums[i],rightmax[i+1]);
+    
+    }    
+    
+    vector<int> ans;
+    for(int i = 0;i<(nums.size()-(k-1));i++)
+        ans[i] = max(rightmax[i],leftmax[i+k-1]);
+    
+    return ans;
+    
+}
