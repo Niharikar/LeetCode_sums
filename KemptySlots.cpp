3e4d5697726804e13f64d98417bd48dbdb3770a4
Niharikar Reddy <niharikar005@gmail.com>
2018-07-05 11:13:27 -0400

    There is a garden with N slots. In each slot, there is a flower. The N flowers will bloom one by one in N days. In each day, there will be exactly one flower blooming and it will be in the status of blooming since then.  Given an array flowers consists of number from 1 to N. Each number in the array represents the place where the flower will open in that day.  For example, flowers[i] = x means that the unique flower that blooms at day i will be at position x, where i and x will be in the range from 1 to N.  Also given an integer k, you need to output in which day there exists two flowers in the status of blooming, and also the number of flowers between them is k and these flowers are not blooming.  If there isn't such day, output -1.
    creating a mapping from positions to days, then first we  consider K positions from between 0 and k+1, to check whether any days of these positions less than days of  0 or K+1.(a)if none of them is less , then we update the answer by taking minimum of ans and max(days[left,days[right]]) and then consider next k elements between k and 2k+1.(b) if any of them is less, then we update the left to next position and right to k+1th position from left.

---
+++ KemptySlots.cpp
@@ -0,0 +1,31 @@
+
+// O(NlogN) time complexity, O(N) space complexity
+int KEmptySlots(vector<int>& flowers,int k)
+{
+    vector<int> days(flowers.size());
+    for(int i = 0;i<flowers.size();i++)
+        days[flowers[i]-1] = i+1; 
+    int left = 0,right = k+1;
+    int ans = INT_MAX;
+    
+   search: 
+    while(right < days.size())
+    {
+        for(int i = left+1;i<right;i++)
+        {
+            if(days[[i]<days[left] || days[i]<days[right])
+            {
+                left = i+1;
+                right = left+k+1;
+                continue search;
+            }
+        }
+        
+        ans = min(ans,max(days[left],days[right]));
+        left = right;
+        right = left+k+1;
+    }
+    
+    return ans < INT_MAX ? ans:-1;
+    
+}
